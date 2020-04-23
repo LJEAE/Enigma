76869abf48e7b5adb5a306c573c3ba80ad6efb42
EPignon <eliott.pignon@ambitiontelecom.com>
2020-04-23 21:54:22 +0200

    BOCOU de chose

---
+++ Assets/Scripts/OpenCadenasCode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCadenasCode : MonoBehaviour
+{
+	public bool isOpen = false;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnMouseDown()
+    {
+		GameObject cube1 = GameObject.Find("CubeCouleur1");
+		GameObject cube2 = GameObject.Find("CubeCouleur2");
+		GameObject cube3 = GameObject.Find("CubeCouleur3");
+		GameObject cube4 = GameObject.Find("CubeCouleur4");
+		
+		if(cube1.GetComponent<ShadeChange>().cubeColor == 1
+			&& cube2.GetComponent<ShadeChange>().cubeColor == 3
+			&& cube3.GetComponent<ShadeChange>().cubeColor == 3
+			&& cube4.GetComponent<ShadeChange>().cubeColor == 2)
+		{
+			isOpen = true;
+			//TODO play anim
+			gameObject.SetActive(false);
+			Debug.Log("CADENAS CODE OUVERT");
+		}
+    }
+}
---
+++ Assets/Scripts/debugclick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugclick : MonoBehaviour
+{
+	public string debug;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnMouseDown()
+    {
+		Debug.Log("CLICK" + debug);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
